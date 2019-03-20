6d296e55cdd053954951a796cbf3c7eae8b5377e
Brittain Hard <brittainhard@gmail.com>
2019-03-20 13:33:00 -0500

    Fixed exercise 116.

--- chapter1/execrises/116.scm
+++ chapter1/execrises/116.scm
@@ -1,8 +1,3 @@
-(define (square x)
-  (* x x))
-
-(define (expt b n)
-  (expt-iter b n 1))
 ;The difference between linear and iterative recursive algorithms is that linear recursive ones pass themselves as an argument, whereas interative ones call themselves to get a more precise return value.
 ;
 ;They all strive towards some base value to return upon, but not before... no no no.
@@ -10,6 +5,12 @@
 ;In the case of the iterative, you are passing the result along to the next function, which then returns it. It's just a for loop without the for.
 ;
 ;The linear goes down to its basest level, but the iterative counts up. The linear will always return something like 1, and then move back up the chain of function calls, but iterative is preparing the return value all the while.
+
+(define (square x)
+  (* x x))
+
+(define (fast-expt b n)
+  (expt-iter b n 1))
 
 (define (even? n)
   (= (remainder n 2) 0))
@@ -23,11 +24,13 @@
   (display "\n")
   (cond ((= counter 0) product)
         ((even? counter) 
-         (expt-iter b 
+         (expt-iter (square b)
                     (/ counter 2)
-                    (square product)))
+                    product))
         (else (expt-iter b
                          (- counter 1)
                          (* b product)))))
 
-(display (expt-iter 3 15 1))
+(display (fast-expt 3 3))
+(display "\n")
+(display (fast-expt 2 4))
---
+++ chapter1/gcd.scm
@@ -0,0 +1,7 @@
+(define (?even x)
+  (= (remainder x 2) 0))
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
